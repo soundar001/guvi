f2944f52461e506c733bc192c3503b197f622682
soundar001 <soundar813@gmail.com>
2019-07-01 18:16:15 +0530

    Create pro19.py

---
+++ pro19.py
@@ -0,0 +1,13 @@
+a=int(input())
+l=[]
+d=[]
+for i in range (0,a):
+        b=(input().split())
+        l.append(b)
+
+for j in range (0,len(l)):
+    for k in range(len(l[j])):
+            d.append(l[j][k])
+d.sort()
+for m in range (0,len(d)):
+    print(d[m],end=" ")
